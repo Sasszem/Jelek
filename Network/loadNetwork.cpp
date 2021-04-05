5d5df9d74c976b16a6b21f7a4121d669d697d033
Sasszem <barath.laszlo.szolnok@gmail.com>
2021-04-06 00:57:56 +0200

    Refactor loading, add shorthands

--- Network/loadNetwork.cpp
+++ Network/loadNetwork.cpp
@@ -16,6 +16,9 @@ using Analyzer::Network::Solvers::NetworkSolverGen;
 using Analyzer::Network::Solvers::NetworkSolverTwoport;
 using Analyzer::Network::Solvers::EquationSystemSolver;
 
+void parseDevice(std::string, std::unique_ptr<Network>&);
+void parseCoupledDevice(std::string, std::unique_ptr<Network>&);
+void parseShorthand(std::string, std::unique_ptr<Network>&);
 
 std::unique_ptr<Network> Analyzer::Network::loadFromStream(std::istream& stream)
 {
@@ -102,91 +105,154 @@ std::unique_ptr<Network> Analyzer::Network::loadFromStream(std::istream& stream)
 
 		if (type[0] == '$') {
 			// coupled devices
-			unsigned id, pPlus, pMinus, coupledId;
-			iss >> id >> pPlus >> pMinus >> coupledId;
-			if (!iss) {
-				throw std::runtime_error(fmt::format("Error: can not parse coupled device base parameters: '{}'", line));
-			}
-
-			if (coupledId == 0 || coupledId > B) {
-				throw std::runtime_error(fmt::format("Error: invalid coupled device ID {}: '{}'", coupledId, line));
-			}
-
-			double param = 0;
-			if (type == "$CCVS" || type == "$CCCS" || type == "$VCVS" || type == "$VCCS") {
-				iss >> param;
-				if (!iss) {
-					throw std::runtime_error(fmt::format("Error: could not parse coupled device parameter: '{}'", line));
-				}
-			}
-
-			if (type == "$CCCS") {
-				device = new CCCS(id, pPlus, pMinus, coupledId, param);
-			}
-			else if (type == "$CCVS") {
-				device = new CCVS(id, pPlus, pMinus, coupledId, param);
-			}
-			else if (type == "$VCVS") {
-				device = new VCVS(id, pPlus, pMinus, coupledId, param);
-			}
-			else if (type == "$VCCS") {
-				device = new VCCS(id, pPlus, pMinus, coupledId, param);
-			}
-			else if (type == "$NULL") {
-				if (coupledId >= id) {
-					throw std::runtime_error(fmt::format("Error: Nullator friend id can not be higher than own id: '{}'", line));
-				}
-				device = new Nullator(id, pPlus, pMinus, coupledId);
-			}
-			else {
-				throw std::runtime_error(fmt::format("Error: unknown coupled device: '{}'", type));
-			}
-
-			network->addDevice(std::unique_ptr<IDevice>(device));
+			parseCoupledDevice(line, network);
 		}
 		else if (type[0] == '!') {
 			// shorthand forms
-
+			parseShorthand(line, network);
 		}
 		else {
 			// normal devices
-
-			unsigned id, pPlus, pMinus;
-			iss >> id >> pPlus >> pMinus;
-			if (!iss) {
-				throw std::runtime_error(fmt::format("Error: can not parse device base parameters: '{}'", line));
-			}
-
-			double param = 0;
-			if (type == "RES" || type == "VOL" || type == "CUR") {
-				iss >> param;
-				if (!iss) {
-					throw std::runtime_error(fmt::format("Error: could not parse device parameter: '{}'", line));
-				}
-			}
-
-			if (type == "RES") {
-				device = new Resistor(id, pPlus, pMinus, param);
-			}
-			else if (type == "VOL") {
-				device = new VoltageSource(id, pPlus, pMinus, param);
-			}
-			else if (type == "CUR") {
-				device = new CurrentSource(id, pPlus, pMinus, param);
-			}
-			else if (type == "WIRE") {
-				device = new Wire(id, pPlus, pMinus);
-			}
-			else if (type == "PROBE") {
-				device = new Probe(id, pPlus, pMinus);
-			}
-			else {
-				throw std::runtime_error(fmt::format("Error: unknown device: '{}'", type));
-			}
-
-			network->addDevice(std::unique_ptr<IDevice>(device));
+			parseDevice(line, network);
+			
 		}
 	}
 
 	return network;
+}
+
+void parseDevice(std::string line, std::unique_ptr<Network>& network) {
+	std::istringstream iss(line);
+	std::string type;
+
+	iss >> type;
+	IDevice* device = nullptr;
+
+	unsigned id, pPlus, pMinus;
+	iss >> id >> pPlus >> pMinus;
+	if (!iss) {
+		throw std::runtime_error(fmt::format("Error: can not parse device base parameters: '{}'", line));
+	}
+
+	double param = 0;
+	if (type == "RES" || type == "VOL" || type == "CUR") {
+		iss >> param;
+		if (!iss) {
+			throw std::runtime_error(fmt::format("Error: could not parse device parameter: '{}'", line));
+		}
+	}
+
+	if (type == "RES") {
+		device = new Resistor(id, pPlus, pMinus, param);
+	}
+	else if (type == "VOL") {
+		device = new VoltageSource(id, pPlus, pMinus, param);
+	}
+	else if (type == "CUR") {
+		device = new CurrentSource(id, pPlus, pMinus, param);
+	}
+	else if (type == "WIRE") {
+		device = new Wire(id, pPlus, pMinus);
+	}
+	else if (type == "PROBE") {
+		device = new Probe(id, pPlus, pMinus);
+	}
+	else {
+		throw std::runtime_error(fmt::format("Error: unknown device: '{}'", type));
+	}
+
+	network->addDevice(std::unique_ptr<IDevice>(device));
+}
+
+void parseCoupledDevice(std::string line, std::unique_ptr<Network>& network) {
+	std::istringstream iss(line);
+	std::string type;
+
+	iss >> type;
+	IDevice* device;
+
+	unsigned id, pPlus, pMinus, coupledId;
+	iss >> id >> pPlus >> pMinus >> coupledId;
+	if (!iss) {
+		throw std::runtime_error(fmt::format("Error: can not parse coupled device base parameters: '{}'", line));
+	}
+
+	if (coupledId == 0 || coupledId > network->B) {
+		throw std::runtime_error(fmt::format("Error: invalid coupled device ID {}: '{}'", coupledId, line));
+	}
+
+	double param = 0;
+	if (type == "$CCVS" || type == "$CCCS" || type == "$VCVS" || type == "$VCCS") {
+		iss >> param;
+		if (!iss) {
+			throw std::runtime_error(fmt::format("Error: could not parse coupled device parameter: '{}'", line));
+		}
+	}
+
+	if (type == "$CCCS") {
+		device = new CCCS(id, pPlus, pMinus, coupledId, param);
+	}
+	else if (type == "$CCVS") {
+		device = new CCVS(id, pPlus, pMinus, coupledId, param);
+	}
+	else if (type == "$VCVS") {
+		device = new VCVS(id, pPlus, pMinus, coupledId, param);
+	}
+	else if (type == "$VCCS") {
+		device = new VCCS(id, pPlus, pMinus, coupledId, param);
+	}
+	else if (type == "$NULL") {
+		if (coupledId >= id) {
+			throw std::runtime_error(fmt::format("Error: Nullator friend id can not be higher than own id: '{}'", line));
+		}
+		device = new Nullator(id, pPlus, pMinus, coupledId);
+	}
+	else {
+		throw std::runtime_error(fmt::format("Error: unknown coupled device: '{}'", type));
+	}
+
+	network->addDevice(std::unique_ptr<IDevice>(device));
+}
+
+void parseShorthand(std::string line, std::unique_ptr<Network>& network) {
+	std::istringstream iss(line);
+	std::string type;
+	
+	iss >> type;
+
+	IDevice* device1;
+	IDevice* device2;
+
+	unsigned id, plus1, minus1, plus2, minus2;
+	iss >> id >> plus1 >> minus1 >> plus2 >> minus2;
+	if (!iss) {
+		throw std::runtime_error(fmt::format("Error: can not parse shorthand base parameters: '{}'", line));
+	}
+
+	double param = 0;
+	if (type == "!TRAN" || type == "!GYR") {
+		iss >> param;
+		if (!iss) {
+			throw std::runtime_error(fmt::format("Error: could not parse shorthand parameter: '{}'", line));
+		}
+	}
+
+	if (type == "!AMP") {
+		device1 = new Wire(id, plus2, minus2);
+		device2 = new Nullator(id + 1, plus1, minus1, id);
+	}
+	else if (type == "!GYR") {
+		device1 = new CCVS(id, plus1, minus1, id + 1, -param);
+		device2 = new CCVS(id + 1, plus2, minus2, id, param);
+	}
+	else if (type == "!TRAN") {
+		device1 = new VCVS(id, plus1, minus1, id + 1, param);
+		device2 = new CCCS(id + 1, plus2, minus2, id, - param);
+	}
+	else {
+		throw std::runtime_error(fmt::format("Error: unknown coupled device: '{}'", type));
+	}
+
+	network->addDevice(std::unique_ptr<IDevice>(device1));
+	network->addDevice(std::unique_ptr<IDevice>(device2));
 }
